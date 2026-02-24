640d0033c03306b1921189c88bc562d8ac0c2d56
ADITHYAN <cb4103317@gmail.com>
2026-02-25 01:52:50 +0530

    Update package.js

--- package.js
+++ package.js
@@ -1,18 +1,38 @@
 {
   "name": "indian-phone-locator",
   "version": "1.0.0",
-  "description": "Indian Phone Number Locator",
+  "description": "Indian Phone Number Locator with Discord Bot - NO API KEY REQUIRED! Uses OpenStreetMap",
   "main": "index.js",
   "scripts": {
-    "start": "node index.js"
+    "start": "node index.js",
+    "dev": "nodemon index.js"
   },
+  "keywords": [
+    "india",
+    "phone",
+    "location",
+    "discord",
+    "bot",
+    "+91",
+    "openstreetmap",
+    "osm",
+    "free",
+    "no-api-key",
+    "gps",
+    "tracker"
+  ],
+  "author": "mrzappu",
+  "license": "MIT",
   "dependencies": {
     "discord.js": "^14.14.1",
     "express": "^4.18.2",
     "axios": "^1.6.2",
     "dotenv": "^16.3.1"
   },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  },
   "engines": {
-    "node": "18.x"
+    "node": ">=18.0.0"
   }
 }
